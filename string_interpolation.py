9dd474cf918caa03c99c8597cc875ee6452e8133
Karely3412 <karely@devpipeline.com>
2024-01-09 23:40:07 -0700

    created string interp func

--- string_interpolation.py
+++ string_interpolation.py
@@ -1,27 +1,27 @@
 # Exsersie
 # - - - - - - - - - - - - - - - - - -
-name = 'Name'
-city = 'City'
-state = 'State'
-age = 'Age'
+# name = 'Name'
+# city = 'City'
+# state = 'State'
+# age = 'Age'
 
-name1 = 'Jane'
-name2 = 'Fred'
-name3 = 'Betsy'
+# name1 = 'Jane'
+# name2 = 'Fred'
+# name3 = 'Betsy'
 
-city1 = 'Boston'
-city2 = 'Portland'
-city3 = 'Orlando'
+# city1 = 'Boston'
+# city2 = 'Portland'
+# city3 = 'Orlando'
 
-state1 = 'Massachusetts'
-state2 = 'Oregon'
-state3 = 'Florida'
+# state1 = 'Massachusetts'
+# state2 = 'Oregon'
+# state3 = 'Florida'
 
-age1 = 24
-age2 = 31
-age3 = 29
+# age1 = 24
+# age2 = 31
+# age3 = 29
 
-dash = '-'
+# dash = '-'
 
 # print(f'{name:<8}{city:<11}{state:<13}{age:>4}')
 # print(f'{dash*7} {dash*10} {dash*13} {dash*4}')
@@ -29,24 +29,50 @@ dash = '-'
 # print(f'{name2:<8}{city2:<10}{state2:<13}{age2:>4}')
 # print(f'{name3:<8}{city3:<10}{state3:<13}{age3:>4}')
 
+    # peoples_list = {
+    #     "name": name,
+    #     "city": city,
+    #     "state": state,
+    #     "age": age
+    # }
+    # peeps_info = f'{header}\n{dashes}\n{person.name:<8}{person.city:<14}{person.state:<14}{person.age:>4}'
 
 
-def people_info(name, city, state, age):
+def people_info(persons_list):
+
     dash = '-'
     header = 'Name' + ' '*4 + 'Ctiy' + ' '*10 + 'State' + ' '*10 + 'Age'
     dashes = f'{dash*6}  {dash*12}  {dash*13}  {dash*4}'
 
-    person_name = name
-    person_city = city
-    person_state = state
-    person_age = age
+    persons_info = ""
+    for person in persons_list:
+        person_info = f'{person[0]:<8}{person[1]:<14}{person[2]:<14}{person[3]:>4}\n'
+        persons_info += person_info   # <-- persons_info = persons_info + person_info (longer version)
+        
 
-    peeps_info = f'{header}\n{dashes}\n{person_name:<8}{person_city:<14}{person_state:<14}{person_age:>4}'
+    peeps_info = f"{header}\n{dashes}\n{persons_info}"
+
 
     return peeps_info
-    
 
-print(people_info('Mickey', 'Aniheim', 'Califonia', "100"))
+
+
+    
+persons_list = [
+    ['Mini', 'Aniheim', 'Califonia', "100"],
+    ['Donald', 'Aniheim', 'Califonia', "100"],
+    ['Mickey', 'Aniheim', 'Califonia', "100"]
+    ]
+
+print(people_info(persons_list))
+
+
+
+
+
+
+# print(people_info('Mini', 'Aniheim', 'Califonia', "100"))
+# print(people_info('Donald', 'Aniheim', 'Califonia', "100"))
 
 
 
