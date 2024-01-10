eb8f3c61931d6dfd7d468c03e62f6fffab5115a7
Karely3412 <karely@devpipeline.com>
2024-01-10 14:39:17 -0700

    made some changes & added some notes

--- string_interpolation.py
+++ string_interpolation.py
@@ -1,33 +1,44 @@
 # Exsersie
+""" 
+You can use:
+name:.2f -> gives money 
+name:<8  -> pushes out way to the right
+name:>8  -> pushes out way to the right
+name:^8  -> centers
+name:08  -> this will give it multiple digets of integers.
+
+
+"""
 # - - - - - - - - - - - - - - - - - -
-# name = 'Name'
-# city = 'City'
-# state = 'State'
-# age = 'Age'
+name = 'Name'
+city = 'City'
+state = 'State'
+age = 'Age'
 
-# name1 = 'Jane'
-# name2 = 'Fred'
-# name3 = 'Betsy'
+name1 = 'Jane'
+name2 = 'Fred'
+name3 = 'Betsy'
 
-# city1 = 'Boston'
-# city2 = 'Portland'
-# city3 = 'Orlando'
+city1 = 'Boston'
+city2 = 'Portland'
+city3 = 'Orlando'
 
-# state1 = 'Massachusetts'
-# state2 = 'Oregon'
-# state3 = 'Florida'
+state1 = 'Massachusetts'
+state2 = 'Oregon'
+state3 = 'Florida'
 
-# age1 = 24
-# age2 = 31
-# age3 = 29
+age1 = 24
+age2 = 31
+age3 = 29
 
-# dash = '-'
+dash = '-'
 
-# print(f'{name:<8}{city:<11}{state:<13}{age:>4}')
-# print(f'{dash*7} {dash*10} {dash*13} {dash*4}')
-# print(f'{name1:<8}{city1:<10}{state1:<13}{age1:>4}')
-# print(f'{name2:<8}{city2:<10}{state2:<13}{age2:>4}')
-# print(f'{name3:<8}{city3:<10}{state3:<13}{age3:>4}')
+print(f'{name:<8}{city:<11}{state:<13}{age:>4}')
+print(f'{dash*7} {dash*10} {dash*13} {dash*4}')
+print(f'{name1:<8}{city1:<10}{state1:<13}{age1:>4}')
+print(f'{name2:<8}{city2:<10}{state2:<13}{age2:>4}')
+print(f'{name3:<8}{city3:<10}{state3:<13}{age3:>4}')
+
 
     # peoples_list = {
     #     "name": name,
@@ -55,8 +66,6 @@ def people_info(persons_list):
 
     return peeps_info
 
-
-
     
 persons_list = [
     ['Mini', 'Aniheim', 'Califonia', "100"],
@@ -70,12 +79,7 @@ print(people_info(persons_list))
 
 
 
-
-# print(people_info('Mini', 'Aniheim', 'Califonia', "100"))
-# print(people_info('Donald', 'Aniheim', 'Califonia', "100"))
-
-
-
+# Needed results:
 
 # Name   City       State         Age
 # ------ ---------- ------------- ----
