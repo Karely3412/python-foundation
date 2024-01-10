5e940ebb17111a094fa95d1dd13d1e90c51af71b
Karely3412 <karely@devpipeline.com>
2024-01-09 22:18:11 -0700

    created regular assignment

---
+++ string_interpolation.py
@@ -0,0 +1,52 @@
+# Exsersie
+# - - - - - - - - - - - - - - - - - -
+name = 'Name'
+city = 'City'
+state = 'State'
+age = 'Age'
+
+name1 = 'Jane'
+name2 = 'Fred'
+name3 = 'Betsy'
+
+city1 = 'Boston'
+city2 = 'Portland'
+city3 = 'Orlando'
+
+state1 = 'Massachusetts'
+state2 = 'Oregon'
+state3 = 'Florida'
+
+age1 = 24
+age2 = 31
+age3 = 29
+
+dash = '-'
+
+print(f'{name:<8}{city:<11}{state:<13}{age:>4}')
+print(f'{dash*7} {dash*10} {dash*13} {dash*4}')
+print(f'{name1:<8}{city1:<10}{state1:<13}{age1:>4}')
+print(f'{name2:<8}{city2:<10}{state2:<13}{age2:>4}')
+print(f'{name3:<8}{city3:<10}{state3:<13}{age3:>4}')
+
+
+
+# def people_info(name, city, state, age):
+
+    # person_
+    # person_
+    # person_
+    # person_
+    
+
+
+print('Name' + ' '*4 + 'Ctiy' + ' '*6 + 'State' + ' '*10 + 'Age')
+
+
+
+
+# Name   City       State         Age
+# ------ ---------- ------------- ----
+# Jane   Boston     Massachusetts  24
+# Fred   Portland   Oregon         31
+# Betsy  Orlando    Florida        29
