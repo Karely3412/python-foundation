69a2261d52601030b7cd68be62887bf20d6eabfb
Karely3412 <karely@devpipeline.com>
2024-01-09 21:02:32 -0700

    added receipt program file & func to it

---
+++ receipt_program.py
@@ -0,0 +1,39 @@
+def customer_receipt():
+
+  meals_ordered = True
+
+  sub_total = 250.87
+
+  tip = sub_total * 0.10
+
+  total = sub_total + tip
+
+  
+
+  if meals_ordered:
+
+    return f'Your total is ${total:.2f}'
+
+    
+
+
+
+print(customer_receipt())
+
+
+# - - - - - - - - - - - - - - - - - - - -
+
+
+
+meals_ordered = True
+
+sub_total = 250.87
+
+tip = sub_total * 0.10
+
+total = sub_total + tip
+
+receipt = "Your total is " + "$" + str(total)
+
+print(receipt)
+
