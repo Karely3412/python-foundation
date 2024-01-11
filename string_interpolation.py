73acc278b48ba6d92907484c1e669185ed174d0a
Karely3412 <karely@devpipeline.com>
2024-01-10 20:55:36 -0700

    updated string interp HW func

--- string_interpolation.py
+++ string_interpolation.py
@@ -1,29 +1,27 @@
 # Homework
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+price_item = {
+    "Purse": 189.00,
+    "Watch": 979.00,
+    "Tie": 9.00
+}
 
-def price_formatter():
-    price_item = {
-        "Purse": 189.00,
-        "Watch": 979.00,
-        "Tie": 9.00
-    }
-
-
+def price_formatter(price_obj):
     formatted_prices = ''
-    for item in price_item:
-        formatted_price = f'{item} {price_item[item]:>8}\n'
+    for item in price_obj:
+        formatted_price = f'{item} {price_obj[item]:>8}\n'
         formatted_prices += formatted_price
             
 
     total = 0
-    for price in price_item:
-            unadded_price = price_item[price]
+    for price in price_obj:
+            unadded_price = price_obj[price]
             total += unadded_price
             
 
-    total_price = f'{formatted_prices}\nTotal price: ${total:.2f}'
+    total_price = f'{formatted_prices}\nTotal price: ${total:,.2f}'
         
     return total_price 
 
 
-print(price_formatter())
+print(price_formatter(price_item))
