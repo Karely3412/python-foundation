c0ed225f4e079b75c1f1a7cf6870c07a6557a61c
Karely3412 <karely@devpipeline.com>
2024-01-10 20:49:09 -0700

    created formatted function for HW

--- excersises.py
+++ excersises.py
@@ -40,7 +40,7 @@ dash = '-'
 
 
 # For the header you can do it like this instead of using a variable because the header names are only being used as is.
-print(f"{'Name':<8}{'City':<11}{'State':<13}{'Age':>4}")
+# print(f"{'Name':<8}{'City':<11}{'State':<13}{'Age':>4}")
 
 # You can also so do:
 # Go over the video to get better detail on this example.
@@ -56,31 +56,31 @@ print(f"{'Name':<8}{'City':<11}{'State':<13}{'Age':>4}")
     # peeps_info = f'{header}\n{dashes}\n{person.name:<8}{person.city:<14}{person.state:<14}{person.age:>4}'
 
 
-def people_info(persons_list):
+# def people_info(persons_list):
 
-    dash = '-'
-    header = 'Name' + ' '*4 + 'Ctiy' + ' '*10 + 'State' + ' '*10 + 'Age'
-    dashes = f'{dash*6}  {dash*12}  {dash*13}  {dash*4}'
+#     dash = '-'
+#     header = 'Name' + ' '*4 + 'Ctiy' + ' '*10 + 'State' + ' '*10 + 'Age'
+#     dashes = f'{dash*6}  {dash*12}  {dash*13}  {dash*4}'
 
-    persons_info = ""
-    for person in persons_list:
-        person_info = f'{person[0]:<8}{person[1]:<14}{person[2]:<14}{person[3]:>4}\n'
-        persons_info += person_info   # <-- persons_info = persons_info + person_info (longer version)
+#     persons_info = ""
+#     for person in persons_list:
+#         person_info = f'{person[0]:<8}{person[1]:<14}{person[2]:<14}{person[3]:>4}\n'
+#         persons_info += person_info   # <-- persons_info = persons_info + person_info (longer version)
         
 
-    peeps_info = f"{header}\n{dashes}\n{persons_info}"
+#     peeps_info = f"{header}\n{dashes}\n{persons_info}"
 
 
-    return peeps_info
+#     return peeps_info
 
     
-persons_list = [
-    ['Mini', 'Aniheim', 'Califonia', "100"],
-    ['Donald', 'Aniheim', 'Califonia', "100"],
-    ['Mickey', 'Aniheim', 'Califonia', "100"]
-    ]
+# persons_list = [
+#     ['Mini', 'Aniheim', 'Califonia', "100"],
+#     ['Donald', 'Aniheim', 'Califonia', "100"],
+#     ['Mickey', 'Aniheim', 'Califonia', "100"]
+#     ]
 
-print(people_info(persons_list))
+# print(people_info(persons_list))
 
 
 
@@ -91,3 +91,107 @@ print(people_info(persons_list))
 # Jane   Boston     Massachusetts  24
 # Fred   Portland   Oregon         31
 # Betsy  Orlando    Florida        29
+
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Excersie -> Inputs
+"""
+When adding inputs make you are casting data types. Make sure you're adding strings to strings or integers to integers.
+
+
+"""
+
+
+
+# def input_func():
+#     question_1 = input (" ")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Excersie -> Data Type Conversons 
+"""
+Notes:
+Go back to video to see the examples again. 
+
+
+"""
+    
+# Implicit:
+     
+num_int = 123
+num_flo = 1.23
+
+num_new = num_int + num_flo
+
+# print("datatype of num_int:",type(num_int))
+# print("datatype of num_flo:",type(num_flo))
+
+# print("Value of num_new:",num_new)
+# print("datatype of num_new:",type(num_new))
+
+
+# Explicit:
+
+# x = 5
+# print('I have ' + str(x) + ' dollars')
+    
+
+# a = 6
+# b = 5.5
+# c = a * b
+# print(int(c))
+# 33
+
+
+# s = "John"
+# k = "95"
+# print(s + k)
+# #John95
+
+
+# g = 3.14159265
+# print(int(g))
+# 3
+
+
+# c = "Done"
+# d = str(100)
+# j = c + d
+# print(j)
+# it'll give you an error if d isn't casted as a string
+
+
+# r = 4.345
+# d = 99.1
+# print(float(r) + float(d))
+# 103.445
+
+# def data_type_conversion(data):
+#     if data:
+
+
+
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
--- string_interpolation.py
+++ string_interpolation.py
@@ -1,3 +1,29 @@
 # Homework
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+def price_formatter():
+    price_item = {
+        "Purse": 189.00,
+        "Watch": 979.00,
+        "Tie": 9.00
+    }
+
+
+    formatted_prices = ''
+    for item in price_item:
+        formatted_price = f'{item} {price_item[item]:>8}\n'
+        formatted_prices += formatted_price
+            
+
+    total = 0
+    for price in price_item:
+            unadded_price = price_item[price]
+            total += unadded_price
+            
+
+    total_price = f'{formatted_prices}\nTotal price: ${total:.2f}'
+        
+    return total_price 
+
+
+print(price_formatter())
