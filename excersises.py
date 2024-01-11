532436d084cfdf7ab01b1cda4e88e60c8d59f52b
Karely3412 <karely@devpipeline.com>
2024-01-11 16:35:21 -0700

    created multiple eight ball funcs

--- excersises.py
+++ excersises.py
@@ -302,35 +302,25 @@ else:
 
 """
 
-score = float(input("Input score: "))
+# score = float(input("Input score: "))
 
-if score >= 90:
-    print("Your score is: A")
-elif score >= 80:
-    print("Your score is: B")
-elif score >= 70:
-    print("Your score is: C")
-elif score >= 60:
-    print("Your score is: D")
-elif score < 50 and score > 0:
-    print("Your score is: F")
-else:
-    print("You've failed.")
-
-
+# if score >= 90:
+#     print("Your score is: A")
+# elif score >= 80:
+#     print("Your score is: B")
+# elif score >= 70:
+#     print("Your score is: C")
+# elif score >= 60:
+#     print("Your score is: D")
+# elif score < 50 and score > 0:
+#     print("Your score is: F")
+# else:
+#     print("You've failed.")
 
 
-user_prompt = input("Ask a question: ")
 
-if score >= 90:
-    print("Your score is: A")
-elif score >= 80:
-    print("Your score is: B")
-elif score >= 70:
-    print("Your score is: C")
-elif score >= 60:
-    print("Your score is: D")
-elif score < 50 and score > 0:
-    print("Your score is: F")
-else:
-    print("You've failed.")
+
+
+
+
+
---
+++ magic_eight_ball.py
@@ -0,0 +1,89 @@
+import random
+
+# def magic_8_ball():
+#     user_prompt = input("Ask a question: ")
+#     rand = random.randrange(1,9)
+
+#     if user_prompt == '':
+#         return ("Program ended")
+    
+#     if rand == 1:
+#         return ("Outlook good!")
+#     elif rand == 2:
+#         return ("You may rely on it!")
+#     elif rand == 3:
+#         return ("Ask aganlater!")
+#     elif rand == 4:
+#         return ("Concentae and ask again!")
+#     elif rand == 5:
+#         return ("Reply hz, try again!")
+#     elif rand == 6:
+#         return ("My repl s no!")
+#     elif rand == 7:
+#         return ("It's certain!")
+#     elif rand == 8:
+#          return ("My sources say no!")
+    
+# print(magic_8_ball())
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -   
+
+
+# def magic_8_ball():
+#     user_prompt = input("Ask a question: ")
+#     rand = random.randrange(1,9)
+
+#     if user_prompt == '':
+#         return ("Program ended")
+    
+#     else:
+#         if rand == 1:
+#             return ("Outlook good!")
+#         elif rand == 2:
+#             return ("You may rely on it!")
+#         elif rand == 3:
+#             return ("Ask aganlater!")
+#         elif rand == 4:
+#             return ("Concentae and ask again!")
+#         elif rand == 5:
+#             return ("Reply hz, try again!")
+#         elif rand == 6:
+#             return ("My repl s no!")
+#         elif rand == 7:
+#             return ("It's certain!")
+#         elif rand == 8:
+#             return ("My sources say no!")
+    
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -   
+
+
+print(magic_8_ball())
+def magic_8_ball():
+    user_prompt = input("Ask a question: ")
+    rand = random.randrange(1,9)
+
+    if user_prompt == '':
+        return ("Program ended")
+    
+    else:
+        if rand == 1:
+            return ("Outlook good!")
+        elif rand == 2:
+            return ("You may rely on it!")
+        elif rand == 3:
+            return ("Ask aganlater!")
+        elif rand == 4:
+            return ("Concentae and ask again!")
+        elif rand == 5:
+            return ("Reply hz, try again!")
+        elif rand == 6:
+            return ("My repl s no!")
+        elif rand == 7:
+            return ("It's certain!")
+        elif rand == 8:
+            return ("My sources say no!")
+    
+
+
+print(magic_8_ball())
