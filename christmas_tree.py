90958a7ad9547b8ffcab927574062e4299dc4095
Karely3412 <karely@devpipeline.com>
2024-01-13 19:59:19 -0700

    updated func christmas tree

--- christmas_tree.py
+++ christmas_tree.py
@@ -5,14 +5,13 @@ def christmas_tree():
     asterisk = '*'
     first_input = input('How many layers tall would you like your tree? ')
     sec_input = input('Virtual elves need a character or symbol to start constructing your tree! ')
-    tree_base = f'{asterisk*3:^30}\n{asterisk*3:^30}'
+    tree_base = f'{asterisk*3:^50}\n{asterisk*3:^50}'
 
     our_tree = '' 
 
     for i in range(0, int(first_input)):
-        our_tree += str(i)
-    
-        print(f'{our_tree:^30}')
+        our_tree += f'{sec_input} '
+        print(f'{our_tree:^50}')
 
 
     print(tree_base)
