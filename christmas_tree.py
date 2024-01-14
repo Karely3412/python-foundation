ba0e5298aa59fcfbbbe5b6befbc91e7e59bf2411
Karely3412 <karely@devpipeline.com>
2024-01-13 19:25:30 -0700

    created .gitignore file & updated christmas tree .py

--- christmas_tree.py
+++ christmas_tree.py
@@ -2,22 +2,28 @@
 
 
 def christmas_tree():
-    # tree_formatter = 
-    user_input = input('How many layers tall would you like your tree? ')
+    asterisk = '*'
+    first_input = input('How many layers tall would you like your tree? ')
+    sec_input = input('Virtual elves need a character or symbol to start constructing your tree! ')
+    tree_base = f'{asterisk*3:^30}\n{asterisk*3:^30}'
+
+    our_tree = '' 
+
+    for i in range(0, int(first_input)):
+        our_tree += str(i)
+    
+        print(f'{our_tree:^30}')
 
 
-    for i in user_input:
-        print (i)
-
-
-
+    print(tree_base)
+    
 
 
 christmas_tree()
 
 
 
-
+# for a str += makes more sense & for a list, .append() makes alot more sense
 
 
 
