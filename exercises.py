860113d80ace352d418525cdc0e273bbbe312e9f
Karely3412 <karely@devpipeline.com>
2024-01-13 22:14:04 -0700

    created leap year file

--- exercises.py
+++ exercises.py
@@ -358,10 +358,9 @@ else:
 
 # nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for i in range(1, 11):
-    if i >= 2 and i <= 7:
-        print(i)
-
+# for i in range(1, 11):
+#     if i >= 2 and i <= 7:
+#         print(i)
 
 
 
---
+++ leap_year.py
@@ -0,0 +1,21 @@
+"""
+
+For this assignment, you will write a program that will prompt the user to enter a year (after 1752 AD)
+
+The program will then print True or False
+
+True, if the year is a leap year
+
+False, if the year is NOT a leap year
+
+A year IS a leap IF:
+
+1) It is divisible by 4,
+
+AND
+
+2) It is NOT divisible by 100,
+
+3) Unless it is also divisible by 400
+
+"""
