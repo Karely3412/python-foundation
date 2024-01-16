54282211215f9e60f123f80615eb002140ce57f9
Karely3412 <karely@devpipeline.com>
2024-01-15 23:02:15 -0700

    have the first formatted line on septembers month

--- month_cal.py
+++ month_cal.py
@@ -1,15 +1,28 @@
-print(f"""        September 2021    
+# print(f"""        September 2021    
 
-   S   M   T   W   T   F   S
+#    S   M   T   W   T   F   S
 
-               1   2   3   4
+#                1   2   3   4
 
-   5   6   7   8   9  10  11
+#    5   6   7   8   9  10  11
 
-  12  13  14  15  16  17  18
+#   12  13  14  15  16  17  18
 
-  19  20  21  22  23  24  25
+#   19  20  21  22  23  24  25
 
-  26  27  28  29  30""")
+#   26  27  28  29  30""")
 
 
+
+def month_of_sept():
+    print('S  M  T  W  T  F  S')
+
+
+    for i in range(1,31):
+        # print(i, end='')
+        if i >= 1 and i <= 4:
+            print(f'{i:>2}', end="")
+
+
+
+month_of_sept()
