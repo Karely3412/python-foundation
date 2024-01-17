cf6b2cc4efbccfe49d4755bb94b4b78e3b2dd5b5
Karely3412 <karely@devpipeline.com>
2024-01-16 19:11:09 -0700

    added to git ignore file

--- christmas_tree.py
+++ christmas_tree.py
@@ -1,27 +1,27 @@
 
 
 
-def christmas_tree():
-    asterisk = '*'
-    first_input = int(input('How many layers tall would you like your tree? '))
-    sec_input = input('Virtual elves need a character or symbol to start constructing your tree! ')
-    tree_base = f'{asterisk*3:^{{first_input}*2}}\n{asterisk*3:^{first_input*2}}'
+# def christmas_tree():
+#     asterisk = '*'
+#     first_input = int(input('How many layers tall would you like your tree? '))
+#     sec_input = input('Virtual elves need a character or symbol to start constructing your tree! ')
+#     tree_base = f'{asterisk*3:^{{first_input}*2}}\n{asterisk*3:^{first_input*2}}'
 
-    while len(sec_input) > 1:
-        sec_input = input('Please only enter one character.')
+#     while len(sec_input) > 1:
+#         sec_input = input('Please only enter one character.')
 
-    our_tree = '' 
+#     our_tree = '' 
 
-    for i in range(0, first_input):
-        our_tree += f'{sec_input} '
-        print(f'{our_tree:^{first_input*2}}')
+#     for i in range(0, first_input):
+#         our_tree += f'{sec_input} '
+#         print(f'{our_tree:^{first_input*2}}')
 
 
-    print(tree_base)
+#     print(tree_base)
     
 
 
-christmas_tree()
+# christmas_tree()
 
 
 
@@ -41,3 +41,19 @@ christmas_tree()
 
    
 """
+
+asterisk = '*'
+first_input = int(input('How many layers tall would you like your tree? '))
+sec_input = input('Virtual elves need a character or symbol to start constructing your tree! ')
+tree_base = f'{asterisk*3:^{{first_input}*2}}\n{asterisk*3:^{first_input*2}}'
+
+while len(sec_input) > 1:
+    sec_input = input('Please only enter one character.')
+
+our_tree = '' 
+
+for i in range(0, first_input):
+    our_tree += f'{sec_input} '
+    print(f'{our_tree:^{first_input*2}}')
+    
+print(tree_base)
