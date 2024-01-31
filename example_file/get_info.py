be6bdf538d8dfdfd3a1ff81825b42cddcf80816e
Karely3412 <karely@devpipeline.com>
2024-01-31 13:50:19 -0700

    created different read file example using with open

--- example_file/get_info.py
+++ example_file/get_info.py
@@ -1,11 +1,21 @@
-a_file = open("example_file/stored_info.txt", "rt")
+# a_file = open("example_file/stored_info.txt", "rt")
 
-file_lines = a_file.readlines()
+# file_lines = a_file.readlines()
 
+# count = 0 
+
+# for line in file_lines:
+#     count += 1 
+#     print(f"Line {count}: {line.strip()}")
+
+# a_file.close()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - -
 count = 0 
 
-for line in file_lines:
-    count += 1 
-    print(f"Line {count}: {line.strip()}")
+with open("example_file/stored_info.txt", "rt") as a_file:
+    for line in a_file:
+        count += 1
+        print(f"Line {count}: {line.strip()}")
 
-a_file.close()
+
