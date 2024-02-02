e84532644af74de5035f517c0bf315961235bb02
Karely3412 <karely@devpipeline.com>
2024-02-01 21:22:21 -0700

    added return to func, empty str & appending to str

--- custom_join.py
+++ custom_join.py
@@ -1,8 +1,36 @@
-def cust_join(it_obj):
-    for i in it_obj:
-        print(i, end='')
+
+def cust_join(it_obj, separ):
+    index_of_list = range(len(it_obj))
+    str_list = ''
+
+    for i in index_of_list:
+        if i == index_of_list[-1]:
+            it_obj_str1 = f'{it_obj[i]}'
+            str_list += it_obj_str1
+        else:
+            it_obj_str2 = f'{it_obj[i]}{separ}' 
+            str_list += it_obj_str2
+            
+    
+            
+    return str_list
+
+
+print(cust_join(["Apple","Banana",15,17.6,"Orange", "Orange"], ','))
 
 
 
 
-cust_join(["Apple","Banana",15,17.6,"Orange"])
+# def cust_join(it_obj, separ):
+#     index_of_list = range(len(it_obj))
+
+#     for i in index_of_list:
+#         if i == len(it_obj) - 1:
+#             it_obj_str = f'{it_obj[i]}'
+#             print(it_obj_str, end='')
+#         else:
+#             it_obj_str = f'{it_obj[i]}{separ}' 
+#             print(it_obj_str, end='')
+
+
+# cust_join(["Apple","Banana",15,17.6,"Orange", "Orange"], ',')
