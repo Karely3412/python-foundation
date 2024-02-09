04e9c740b145149aab5cf780ebab3ce1d75183b5
Karely3412 <karely@devpipeline.com>
2024-02-08 20:19:29 -0700

    created read csv_file.py

---
+++ csv_files/csv/csv_file.py
@@ -0,0 +1,4 @@
+with open("csv_files/csv/countydata.csv") as a_file:
+    read_data = a_file.read()
+
+    print(read_data)
--- slice_function.py
+++ slice_function.py
@@ -1,8 +1,19 @@
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+"""
+
+1. iter_obj: The iterable object that will be split. (Required)
+2. start: The start index. Negative numbers are valid. Default=1
+3. stop: The stop index. Remember that the slice will end before the stop index. Default = None
+4. step: The step direction and magnitude as a positive or negative integer. Default=1
+
+"""
 
 
-def slice(iter_obj):
-    return iter_obj
+
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+# def slice(iter_obj):
+#     return iter_obj
 
 
 
@@ -10,8 +21,31 @@ def slice(iter_obj):
 
 
 
-print(slice(my_list))
+# print(slice(my_list))
 
 
-print( nums[1:-2:-1] )
-print( slice(nums, 1, -2, -1) )
+# print( nums[1:-2:-1] )
+# print( slice(nums, 1, -2, -1) )
+
+
+
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# output = slice(nums, 2, -2)
+
+# print(output)
+
+# # Output
+# # [3, 4, 5, 6, 7]
+
+# ## Example 2
+# print( slice(nums, stop = 4) )
+
+# # Output
+# # [1, 2, 3, 4]
+
+# ## Example
+# print( slice(nums, step=-1) )
+
+# Output
+# [9, 8, 7, 6, 5, 4, 3, 2, 1]
