8cd6b018bfd4acc10fa071198d76375d3f895046
Karely3412 <karely@devpipeline.com>
2024-02-08 20:50:22 -0700

    slpit csv file

--- csv_files/csv/csv_file.py
+++ csv_files/csv/csv_file.py
@@ -1,4 +1,12 @@
 with open("csv_files/csv/countydata.csv") as a_file:
     read_data = a_file.read()
+    list_data = []
 
-    print(read_data)
+    split_data = read_data.split(",")
+    list_data.append(split_data)
+    print(list_data)
+
+    # for row in list_data:
+    #     print(row)
+
+    
