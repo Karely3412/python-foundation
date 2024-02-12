fa0bb32f67ae45bcb0f3c78cbd2015e2d0e5e3f8
Karely3412 <karely@devpipeline.com>
2024-02-11 21:51:27 -0700

    finished csv_file.py

--- csv_files/csv/csv_file.py
+++ csv_files/csv/csv_file.py
@@ -3,16 +3,24 @@ with open("csv_files/csv/countydata.csv") as a_file:
 
     header = a_file.readline()
     split_data = header.strip().split(",")
-    print(f'{split_data[0]:20} {split_data[1]:>8} {split_data[-1]:>8}')
+    print(f'{split_data[0]:17} {split_data[1]:>9} {split_data[-1]:>8} {"Growth":>8} {"Rate":>9}')
     print('----------------- -------- -------- -------- ---------')
 
     for i in a_file:
-        split_data = i.split(",")
+        split_data = i.strip().split(",")
         list_data.append((split_data[0], split_data[1], split_data[-1]))
 
+
     for x in list_data:
-        print(f'{x[0]:17} {x[1]:>8} {x[-1]:>9}', end="")
+        growth_calc = int(x[-1]) - int(x[1])
+        percentage = (growth_calc/int(x[1])) * 100
+        formatted_rate = f'{percentage:.2f}'
+        
+  
+        print(f'{x[0]:17} {x[1]:>8} {x[-1]:>8} {growth_calc:>8} {formatted_rate:>9} %')
 
     
+    
+
 
     
