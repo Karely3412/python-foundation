f3b935e0a4f9ca6be1acf08b5ef1e66c5594fe01
Karely3412 <karely@devpipeline.com>
2024-02-11 20:43:36 -0700

    extracted & formatted data

--- csv_files/csv/csv_file.py
+++ csv_files/csv/csv_file.py
@@ -1,11 +1,17 @@
 with open("csv_files/csv/countydata.csv") as a_file:
     list_data = []
 
+    header = a_file.readline()
+    split_data = header.strip().split(",")
+    print(f'{split_data[0]:20} {split_data[1]:>8} {split_data[-1]:>8}')
+    print('----------------- -------- -------- -------- ---------')
+
     for i in a_file:
         split_data = i.split(",")
-        joined_data = " ".join((split_data[0], split_data[1], split_data[-1]))
-        list_data.append(joined_data)
-    print(list_data)
+        list_data.append((split_data[0], split_data[1], split_data[-1]))
+
+    for x in list_data:
+        print(f'{x[0]:17} {x[1]:>8} {x[-1]:>9}', end="")
 
     
 
