1702cecb3474eecfbe337515434d0f01f1105215
Karely3412 <karely@devpipeline.com>
2024-03-04 16:45:31 -0700

    finished calendar functions

--- cal_funcs.py
+++ cal_funcs.py
@@ -2,45 +2,35 @@ def is_leap_year(year):
     return (year % 4 == 0) and not year % 100 == 0 or year % 400 == 0
 
 
-# def print_cal_month(month, year, start_day_of_week):
-#     title = f"        {month} {year}\n"
-#     print(f'{title}')
-#     week_days = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+def print_cal_month(month, year, start_day_of_week, days_in_months):
+    title = f"        {month} {year}\n"
+    week_days = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
 
-#     for i in range(0,6):    
-#        print(i)
+    print(f'{title}')
+
+    for day in week_days:
+        print(f'{day:>2}  ', end='')
+    print('\n')
+    
+    count = 0
+
+    for i in range(1 - start_day_of_week, days_in_months + 1):
+        count += 1
+
+        if i >= 1:
+            print(f'{i:>2}  ', end='')
+        if i < 1:
+            print(f'{" "*4}', end='')
+
+        if count % 7 == 0:
+            print('\n')
+    print()
             
     
 
-
-
-#     # for i in range(1,31):
-#     #     if i == 5 or i == 12 or i == 19 or i == 26:
-#     #         print("\n")
-#     #     print(f'{i:>4}', end="")
-
-#     print()
+print(is_leap_year(2024))
+print_cal_month("February", "2024", 0, 29)
 
 
 
-# is_leap_year()
-# print_cal_month('january', 2023)
-
-
-# start_day_of_week, days_in_month
-
-"""
-
-
-
-
-"""
-
-
-user_input = 3
-first_week = [1, 2, 3, 4, 5, 6, 7]
-
-for i in first_week:
-    if i in list:    
-       print(i)
             
