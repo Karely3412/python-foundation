618493c1ad271cc3690a24662d11588f9c23072e
Karely3412 <karely@devpipeline.com>
2024-03-04 15:04:56 -0700

    finished csv module project

--- csv_files/csv_module/csv_module.py
+++ csv_files/csv_module/csv_module.py
@@ -9,37 +9,43 @@ with open('csv_files/csv_module/states-agegroup-2020.csv', 'r') as read_data:
     for i in new_data:
         my_list.append(i)
 
-    header = my_list[0][0]
-    header_field = my_list[0][8]
+    # print(my_list)
+
+
+
+
+with open('csv_files/csv_module/csv_mod_data.csv', 'w') as data:
+    new_data = csv.writer(data)
+    csv_list = []
+   
+    for row in my_list:
+        temp_row = [row[0], row[8], row[10], row[3], row[6]]
+        csv_list.append(temp_row)
+        
+    new_data.writerows(csv_list)
+            
+
+
+
+        
+with open('csv_files/csv_module/csv_mod_data.csv', 'r') as read_data:
+    new_data = csv.reader(read_data)
+    headers = next(new_data)
+
     dash = '-'
 
-    print (f'\n{header} {header_field}')
-    print(f'{dash*len(header)} {dash*len(header_field)}\n\n{my_list[5][0]} {my_list[5][8]}\n')
+    print(f'\n{headers[0]:<13} {headers[1]:<45} {headers[2]:<50} {headers[3]:<45} {headers[4]}\n')
+    # print(f'{dash*len(headers[0]):<13} {dash*len(headers[1]):<13} {dash*len(headers[2]):<13} {dash*len(headers[3]):<13} {dash*len(headers[4]):<13}')
 
-
-    
-    # if len(header_field):
-    #     print(header_field)
-    # for i in my_list[0]:
-    #     print(i)
-    
-    # for i in my_list:
-    #     if i[0] == 'Utah':
-    #         print(i[0])
+    for row in new_data:
+        print(f'{row[0]:<25} {row[1]:>30} {row[2]:>48} {row[3]:>45} {row[4]:>32}')
 
 
 
     
+     
 
 
-
-
-# with open('csv_files/csv_module/nation-agegroup-2020.csv', 'w') as data:
-#     new_data = csv.writer(data)
-
-#     new_data.writerows(rows)
-
-    
     
 
 
