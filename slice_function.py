36ca3b274c4c3a1299d44239e55b60d2a0accb7d
Karely3412 <karely@devpipeline.com>
2024-04-07 21:40:11 -0600

    added start & stop in slice function

--- slice_function.py
+++ slice_function.py
@@ -2,6 +2,10 @@
 
 1. iter_obj: The iterable object that will be split. (Required)
 2. start: The start index. Negative numbers are valid. Default=1
+    - For Loop 
+    - Start parameter
+    - Conditional
+
 3. stop: The stop index. Remember that the slice will end before the stop index. Default = None
 4. step: The step direction and magnitude as a positive or negative integer. Default=1
 
@@ -9,14 +13,38 @@
 
 
 
-# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
-# def slice(iter_obj):
-#     return iter_obj
+def slice(iter_obj, start, stop):
+    # print(iter_obj)
+    sliced_list = []
+    # print(start)
+    iter_obj_start = iter_obj[start]-1
+
+    for i in iter_obj:
+        if i >= start and i <= stop:
+            print(i)
+
+        
+
+        
+
+        
+        
+            
+            
+
+        
 
 
 
+
+
+    # return iter_obj
+
+
+slice(nums, 5, 8)
 
 
 
