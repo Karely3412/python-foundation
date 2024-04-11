d093af4b8cb5c0b2c545f36d9428bbbd2b958a3d
Karely3412 <karely@devpipeline.com>
2024-04-11 14:06:34 -0600

    created menu_func

---
+++ SQLite_python.py
@@ -0,0 +1,47 @@
+"""
+How may functions are needed?
+    - Get all func
+    - Add customer
+    - Update customer info
+    - Search for one 
+    - Delete by cust id
+    - Print menu func
+
+
+1. Print menu options
+2. View all Customers in a table
+3. Search for Customers by name (partial name)
+4. Add a new Customer to the database
+5. Update a Customer record (like changing their phone number)
+6. Delete a Customer using their customer_id
+7. Quit
+
+
+
+"""
+
+import sqlite3
+
+conn =  sqlite3.connect('dp_customers.db')
+
+cursor = conn.cursor()
+
+def customer_menu():
+    print('\n**** Customer Database ****\n')
+    menu_options = ["[1] View All Customers", "[2] Search Customers", "[3] Add a New Customer", "[Q] Quit\n"]
+
+    for i in menu_options:
+        print(i)
+
+
+def 
+
+
+
+
+
+
+customer_menu()
+
+
+
